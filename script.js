4a163dd2086dafb6f68090de4368561cf297ab20
Piotr Gaik <piotgie@gmail.com>
2021-01-15 19:49:42 +0100

    mp3 player

---
+++ script.js
@@ -0,0 +1,133 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
+const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+
+// music
+const songs = [
+    {
+        name: 'polog',
+        displayName: 'I Know',
+        artist: 'Polo G',
+    },
+    {
+        name: 'popsmoke',
+        displayName: 'Invincible',
+        artist: 'Pop Smoke',
+    },
+    {
+        name: 'novacane',
+        displayName: 'Novacane',
+        artist: 'Frank Ocean',
+    },
+    {
+        name: 'zeus',
+        displayName: 'Zeus',
+        artist: 'Eminem',
+    }
+];
+
+// check if playing
+let isPlaying = false;
+
+// Play 
+function playSong() {
+    isPlaying = true;
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause');
+    music.play();
+}
+
+// Pause 
+function pauseSong() {
+    isPlaying = false;
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
+    music.pause();
+}
+
+// play or pause event listener
+playBtn.addEventListener('click', ()=> (isPlaying ? pauseSong() : playSong()));
+
+// update DOM
+function loadSong(song) {
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+}
+
+// current song
+let songIndex = 0;
+
+// prev song
+function prevSong() {
+    songIndex--;
+    if(songIndex < 0){
+        songIndex = songs.length -1
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// next song
+function nextSong() {
+    songIndex++;
+    if(songIndex > songs.length -1) {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// on load - select first song
+loadSong(songs[songIndex]);
+
+// update progress bar & time
+function updateProgressBar(e){
+    if (isPlaying) {
+        const {duration, currentTime} = e.srcElement;
+        // update progress bar width
+        const progressPercent = (currentTime / duration) * 100;
+        progress.style.width = `${progressPercent}%`;
+        // calculate display for duration
+        const durationMinutes = Math.floor(duration / 60);
+        let durationSeconds = Math.floor(duration % 60); // co zostanie po 60
+        if(durationSeconds < 10) {
+            durationSeconds = `0${durationSeconds}`;
+        }
+        
+        // delay switching duration element to avoid NaN
+        if (durationSeconds) {
+            durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+        }
+        // calculate display for current
+        const currentMinutes = Math.floor(currentTime / 60);
+        let currentSeconds = Math.floor(currentTime % 60); // co zostanie po 60
+        if(currentSeconds < 10) {
+            currentSeconds = `0${currentSeconds}`;
+        }
+        currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`
+    }
+};
+
+// set progress bar
+function setProgressBar(e) {
+    const width = this.clientWidth; // eventy ktore chcemy uzyc to this eventy
+    const clickX = e.offsetX; // e targety to te ktore namierzamy
+    const {duration} = music;
+    music.currentTime = (clickX / width) * duration;
+};
+
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+music.addEventListener('ended', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar);
